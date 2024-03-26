cae0cf7e7a923ff7a545b0759ef4dfb639ac191b
Mudasir Chandio <mudasirc046@gmail.com>
2024-03-25 21:11:08 -0700

    created simple-calculator

---
+++ index.ts
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+
+const answar = await inquirer.prompt([
+  { message: "Enter your first number: ", type: "number", name: "firstNumber" },
+  {
+    message: "Enter your second number: ",
+    type: "number",
+    name: "secondNumber",
+  },
+  {
+    message: "Please select one of the operator to perform the action",
+    type: "list",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    name: "operator",
+  },
+]);
+// Conditional Statements
+if (answar.operator === "Addition") {
+  console.log(answar.firstNumber + answar.secondNumber);
+} else if (answar.operator === "Subtraction") {
+  console.log(answar.firstNumber - answar.secondNumber);
+} else if (answar.operator === "Multiplication") {
+  console.log(answar.firstNumber * answar.secondNumber);
+} else if (answar.operator === "Division") {
+  console.log(answar.firstNumber / answar.secondNumber);
+}
